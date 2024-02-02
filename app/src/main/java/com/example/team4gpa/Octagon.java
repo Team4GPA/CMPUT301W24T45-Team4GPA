564f804a9b38884436ffba37c296dfca1040a2a8
Chase Lloyd <chaseufc2010@gmail.com>
2024-02-01 18:06:13 -0700

    Create Octagon.java

---
+++ app/src/main/java/com/example/team4gpa/Octagon.java
@@ -0,0 +1,10 @@
+package com.example.team4gpa;
+
+public class Octagon extends Shape{
+
+    private int area; // Area of Octagon
+
+    private int id; // ID for shape
+
+    private String name; // Name of shape
+}
